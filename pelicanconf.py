935f0092f253d6f665b9c405f759a726b05fc185
Boddapati balaji <boddapatibalaji666@gamil.com>
2025-06-25 10:51:40 +0530

    Initial Pelican blog setup

---
+++ pelicanconf.py
@@ -0,0 +1,18 @@
+AUTHOR = 'Balaji'
+SITENAME = 'Balaji Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+DEFAULT_LANG = 'en'
+
+THEME = 'notmyidea'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+
+DEFAULT_PAGINATION = 10
+
+RELATIVE_URLS = True
---
+++ publishconf.py
@@ -0,0 +1,8 @@
+from pelicanconf import *
+
+SITEURL = 'https://Boddapatibalaji.github.io/balaji-techdevops-blog'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+DELETE_OUTPUT_DIRECTORY = True
